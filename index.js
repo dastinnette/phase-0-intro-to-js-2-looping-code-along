88095efc0cccfad34bf4845f82b824b321c0d544
David Stinnette <dastinnette@gmail.com>
2023-08-26 15:24:19 -0400

    Completed looping code along

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(guests, gift) {
+    const cards = []
+
+    for (let i = 0; i < guests.length; i++) {
+        cards.push(`Thank you, ${guests[i]}, for the wonderful ${gift} gift!`)
+    }
+
+    return cards 
+}
+
+function countDown(n) {
+    let count = n
+    while (count >= 0) {
+        console.log(count--)
+    }
+}
